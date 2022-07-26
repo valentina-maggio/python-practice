da347e39d76b413776589ce4002d3b177bd711db
Valentina <maggio.vale@gmail.com>
2022-07-26 12:31:48 +0100

    Add maths exercise

--- python-basics/datatypes.py
+++ python-basics/datatypes.py
@@ -76,3 +76,22 @@ print(result) # Returns 1.0
 score = 0
 isWinning = false
 print(f"your score is {score}, you are winning is {isWinning}") # Returns your score is 0, you are winning is false
+
+
+# Exercise 3
+# Create a program using maths and f-Strings that tells us how many days, weeks, months we have left if we live until 90 years old.
+# It will take your current age as the input and output a message with our time left in this format:
+# You have x days, y weeks, and z months left.
+# Where x, y and z are replaced with the actual calculated numbers.
+
+# Don't change the code below
+age = input("What is your current age?")
+# Don't change the code above
+
+#Write your code below this line
+
+years = 90 - int(age)
+months = years * 12
+weeks = years  * 52
+days = years * 365
+print(f"You have {days} days, {weeks} weeks, and {months} months left")
